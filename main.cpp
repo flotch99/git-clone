52127b3f772a758bd66689316c70fcfae53552c0
Fletcher <a1749652@adelaide.edu.au>
2025-06-18 19:12:22 +0930

    Add directory structure and main function

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+//The main function of the code, will take user inputs in the form of argc and argv (learnt from geeksforgeeks.org).
+int main(int argc, char* argv[]) {
+    
+
+}
